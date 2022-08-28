4b9898414a53f2aebd0884c5537a6371afa6236e
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-08-28 19:52:12 +0900

    feat: add input field

---
+++ src/components/atoms/InputField/index.stories.js
@@ -0,0 +1,7 @@
+import InputField from './index.vue'
+
+export default {
+  component: InputField,
+}
+
+export const Default = {}
---
+++ src/components/atoms/InputField/index.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="input"></div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'InputField',
+})
+</script>
+
+<style lang="scss" scoped>
+.input {
+  width: 100%;
+  height: 100vh;
+  border: 1px solid #ccc;
+}
+</style>
