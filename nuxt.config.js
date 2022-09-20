dffb6229fc6a448df4b99ad8d404b2c081f50ab9
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-09-20 23:53:55 +0900

    chore: migration for Vue3

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,4 +66,22 @@ export default {
       },
     },
   },
+
+  chainWebpack: (config) => {
+    config.resolve.alias.set('vue', '@vue/compat')
+
+    config.module
+      .rule('vue')
+      .use('vue-loader')
+      .tap((options) => {
+        return {
+          ...options,
+          compilerOptions: {
+            compatConfig: {
+              MODE: 2,
+            },
+          },
+        }
+      })
+  },
 }
