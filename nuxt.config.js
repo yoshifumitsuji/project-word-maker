4fb211f28d7f0084d8b90a200d45c5c92b5ca77f
yoshifumitsuji <yoshifumi.tsuji.biz@gmail.com>
2022-09-26 12:27:45 +0900

    fix: delete unused code

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,22 +66,4 @@ export default {
       },
     },
   },
-
-  chainWebpack: (config) => {
-    config.resolve.alias.set('vue', '@vue/compat')
-
-    config.module
-      .rule('vue')
-      .use('vue-loader')
-      .tap((options) => {
-        return {
-          ...options,
-          compilerOptions: {
-            compatConfig: {
-              MODE: 2,
-            },
-          },
-        }
-      })
-  },
 }
